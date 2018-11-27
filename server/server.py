40d9630cb6add64926aeaf89fdaa364246dd6935
jpucilos <jpucilos@stevens.edu>
2018-11-27 11:39:27 -0500

    Attempt to add database management to program

--- server/server.py
+++ server/server.py
@@ -2,13 +2,14 @@
 from socket import AF_INET, socket, SOCK_STREAM
 from threading import Thread
 import sys
+import pymongo
 
 def accept_incoming_connections():
     """Sets up handling for incoming clients."""
     while True:
         client, client_address = SERVER.accept()
         print("%s:%s has connected." % client_address)
-        client.send(bytes("Greetings from the cave! Now type your name and press enter!\n", "utf8"))
+        client.send(bytes("Hello! Type your name and press enter!\n", "utf8"))
         addresses[client] = client_address
         Thread(target=handle_client, args=(client,)).start()
 
@@ -17,17 +18,31 @@ def handle_client(client):  # Takes client socket as argument.
     """Handles a single client connection."""
 
     name = client.recv(BUFSIZ).decode("utf8")
-    welcome = 'Welcome %s!' % name
-    client.send(bytes(welcome, "utf8"))
+    entry_key = ""
+    if messages.find_one({"name": name}) != None:
+        client.send(bytes("Please enter your entry key" , "utf8")  + bytes("\n"))
+        find_password = messages.find_one({"name": name}, {"_id": 0, "entry_key": 1})
+        print( find_password )
+    else:
+        welcome = 'Welcome %s!' % name
+        client.send(bytes(welcome + " Please enter an entry key for future use.", "utf8") + bytes("\n"))
+        entry_key = client.recv(BUFSIZ).decode("utf8")
+        
+        
+        
+
     msg = "%s has joined the chat!" % name
     print(msg)
     broadcast(bytes(msg, "utf8"))
+    messages.insert_one({"name": name, "entry_key": entry_key, "message" : msg})
+    
     clients[client] = name
 
     while True:
         msg = client.recv(BUFSIZ)
         if msg != bytes("{quit}", "utf8"):
             broadcast(msg, name+": ")
+            messages.insert_one({"name": name, "entry_key": entry_key, "message" : msg})
         else:
             #client.send(bytes("{quit}", "utf8"))
             client.close()
@@ -44,10 +59,16 @@ def broadcast(msg, prefix=""):  # prefix is for name identification.
     for sock in clients:
         sock.send(bytes(prefix, "utf8")+msg+bytes("\n"))
 
-        
+#Pymongo Set-up        
 clients = {}
 addresses = {}
+myClient = pymongo.MongoClient("mongodb://localhost:27017/")
+myDatabase = myClient["server_database"]
+print("We made it this far")
 
+messages = myDatabase["messages"]
+	
+#Socket set-up
 BUFSIZ = 1024
 ADDR = (sys.argv[1], 10000)
 SERVER = socket(AF_INET, SOCK_STREAM)
